9a62319a00da9ab4a2296cd6aaf58bf976c8080f
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2022-01-11 11:30:58 +0545

    update total_documents in output in read_users API

--- src/functions/profile/read_users.py
+++ src/functions/profile/read_users.py
@@ -16,9 +16,11 @@ def read_users_main(event, context):
         offset, limit = skiplimit(page_size, page_num)
 
         users = User.objects.skip(offset).limit(limit)
+        user_count = users.count()
 
         response_body["data"] = json.loads(users.to_json())
-        response_body["total_page_num"] = math.ceil(users.count() / page_size)
+        response_body["total_page_num"] = math.ceil(user_count / page_size)
+        response_body["total_documents"] = user_count
         response_body["msg"] = "User query successfully"
         response_body["status"] = True
         status_code = 200
