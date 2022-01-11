6ced39c4de1ae3931433b9522b284530529d8369
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2022-01-11 10:39:52 +0545

    add pagination

--- src/functions/profile/read_users.py
+++ src/functions/profile/read_users.py
@@ -7,7 +7,13 @@ from src.controller.cors import response
 def read_users_main(event, context):
     response_body = {}
     try:
-        response_body["data"] = json.loads(User.objects().to_json())
+        parameters = event["queryStringParameters"]
+        limit = int(parameters.pop("limit", 10))
+        offset = int(parameters.pop("offset", 0))
+        # * limit means items per page
+        # * offset means skip that many rows before beginning to return rows
+        users = User.objects.skip(offset).limit(limit)
+        response_body["data"] = json.loads(users.to_json())
         response_body["msg"] = "User query successfully"
         response_body["status"] = True
         status_code = 200
